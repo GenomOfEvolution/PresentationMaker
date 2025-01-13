0711eae0c586dcf174ee6d58cf21c0670c711983
Oleg Nechaev <oleg_nechaev_2019@mail.ru>
2025-01-13 16:55:50 +0300

    SlideList fixes

--- src/components/imageButton/ImageButton.tsx
+++ src/components/imageButton/ImageButton.tsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
 import styles from "./ImageButton.module.css";
-import ModalWindow from "../modalWindow/modalWindow";
 
 export type ImageButtonProps = {
   onClick: () => void;
@@ -9,29 +7,11 @@ export type ImageButtonProps = {
 };
 
 const ImageButton = (props: ImageButtonProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleConfirm = () => {
-    props.onClick();
-    setIsModalOpen(false);
-  };
-
   let styleName: string = "material-icons " + props.imageName;
   return (
-    <>
-      <button onClick={handleClick} className={styles.ImageButton} title={props.title}>
-        <i className={styleName}>{props.imageName}</i>
-      </button>
-      {isModalOpen && <ModalWindow onCancel={handleCancel} onConfirm={handleConfirm} />}
-    </>
+    <button onClick={props.onClick} className={styles.ImageButton} title={props.title}>
+      <i className={styleName}>{props.imageName}</i>
+    </button>
   );
 };
 
